96cf030e989d37bdc42fa8b6ca8bd2cd33ec9d67
Gordei2 <76713351+Gordei2@users.noreply.github.com>
2024-03-20 19:33:55 +0700

    Added comments

--- kemsu-vue-app/src/services/StudentService.ts
+++ kemsu-vue-app/src/services/StudentService.ts
@@ -3,28 +3,33 @@ import axios from 'axios'
 const STUDENT_API_BASE_URL = "http://localhost:8080/api/students"
 
 class StudentService{
+    // Получить список всех студентов
     getStudents(){
         return axios.get(STUDENT_API_BASE_URL);
     }
 
+    // Получить конкретного студента
     getStudent(id){
         return axios.get(STUDENT_API_BASE_URL, {
             params: { studentId: id }
         });
     }
 
+    // Обновить данные о студенте
     updateStudent(id, student: Student){
         axios.put(STUDENT_API_BASE_URL, {
             params: { studentId: id, data: student }
         });
     }
 
+    // Создать нового студента
     createStudent(student: Student){
         axios.post(STUDENT_API_BASE_URL, {
             params: { data: student }
         })
     }
 
+    // Удалить конкретного студента
     removeStudent(id){
         axios.delete(STUDENT_API_BASE_URL, {
             params: { studentId: id }
--- kemsu-vue-app/src/services/UserServices.ts
+++ kemsu-vue-app/src/services/UserServices.ts
@@ -3,12 +3,14 @@ import axios from 'axios'
 const USER_API_BASE_URL = "http://localhost:8080/api/users"
 
 class UserService{
+    // Аутентифицировать пользователя
     auth(user: User){
-        return axios.get(USER_API_BASE_URL. {
+        return axios.get(USER_API_BASE_URL, {
             params: { user: User }
         });
     }
 
+    // Выйти из учетной записи
     exit(id){
         return axios.get(USER_API_BASE_URL, {
             params: { studentId: id }
