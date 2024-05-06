42cc4f880b4d65fc484f986a4493161d5530ceaa
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2024-05-06 20:43:39 +0700

    Fixed new controller with custom swagger annotation

--- backend/src/main/kotlin/org/example/migapi/domain/controller/StudentController.kt
+++ backend/src/main/kotlin/org/example/migapi/domain/controller/StudentController.kt
@@ -1,7 +1,6 @@
 package org.example.migapi.domain.controller
 
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -25,99 +24,40 @@ class StudentController(
     @GetMapping
     @Operation(
         summary = "Получить список всех студентов",
-        description = "Администратор запрашивает список всех студентов",
-        responses = [
-            ApiResponse(
-                responseCode = "200",
-                description = "Список получен",
-                content = [Content(array = ArraySchema(schema = Schema(implementation = StudentDto::class)))]
-            ),
-            ApiResponse(
-                responseCode = "401",
-                description = "Unauthorized",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            ),
-            ApiResponse(
-                responseCode = "403",
-                description = "Forbidden",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            ),
-            ApiResponse(
-                responseCode = "500",
-                description = "Internal server error",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            )
-        ]
+        description = "Администратор запрашивает список всех студентов"
     )
+    @ApiResponse(responseCode = "200", description = "Получен список студентов")
+    @CommonApiErrors
     @SecurityRequirement(name = "JWT")
     fun getAll(): List<StudentDto> = studentService.getAll()
 
     @GetMapping("{id}")
     @Operation(
         summary = "Получить конкретного студента по id",
-        description = "Администратор запрашивает запись студента по его id",
-        responses = [
-            ApiResponse(
-                responseCode = "200",
-                description = "Студент получен",
-                content = [Content(schema = Schema(implementation = StudentDto::class))]
-            ),
-            ApiResponse(
-                responseCode = "404",
-                description = "Not found",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            ),
-            ApiResponse(
-                responseCode = "401",
-                description = "Unauthorized",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            ),
-            ApiResponse(
-                responseCode = "403",
-                description = "Forbidden",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            ),
-            ApiResponse(
-                responseCode = "500",
-                description = "Internal server error",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            )
-        ]
+        description = "Администратор запрашивает запись студента по его id"
     )
+    @ApiResponse(responseCode = "200", description = "Студент получен")
+    @ApiResponse(
+        responseCode = "404",
+        description = "Студент не найден",
+        content = [Content(schema = Schema(implementation = Error::class))]
+    )
+    @CommonApiErrors
     @SecurityRequirement(name = "JWT")
     fun getById(@PathVariable id: String): StudentDto = studentService.getById(id)
 
     @PutMapping
     @Operation(
         summary = "Изменить запись студента",
-        description = "Администратор сохраняет отредактированную запись студента",
-        responses = [
-            ApiResponse(
-                responseCode = "200",
-                description = "Запись сохранена"
-            ),
-            ApiResponse(
-                responseCode = "404",
-                description = "Not found",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            ),
-            ApiResponse(
-                responseCode = "401",
-                description = "Unauthorized",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            ),
-            ApiResponse(
-                responseCode = "403",
-                description = "Forbidden",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            ),
-            ApiResponse(
-                responseCode = "500",
-                description = "Internal server error",
-                content = [Content(schema = Schema(implementation = Error::class))]
-            )
-        ]
+        description = "Администратор сохраняет отредактированную запись студента"
     )
+    @ApiResponse(responseCode = "200", description = "Запись изменена или сохранена")
+    @ApiResponse(
+        responseCode = "404",
+        description = "Студент не найден",
+        content = [Content(schema = Schema(implementation = Error::class))]
+    )
+    @CommonApiErrors
     @SecurityRequirement(name = "JWT")
     fun put(studentDto: StudentDto) = studentService.put(studentDto)
 
