3ca5d401261ce4f01c09cb705e8cc00ee2b8c065
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2024-05-19 09:36:17 +0700

    ы

--- backend/src/main/kotlin/org/example/migapi/domain/notification/controller/NotificationController.kt
+++ backend/src/main/kotlin/org/example/migapi/domain/notification/controller/NotificationController.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @PreAuthorize("hasRole('ROLE_USER')")
-@RequestMapping("/api/notificationы")
+@RequestMapping("/api/notifications")
 class NotificationController(
     @Autowired
     private val firebaseTokenService: FirebaseTokenService
