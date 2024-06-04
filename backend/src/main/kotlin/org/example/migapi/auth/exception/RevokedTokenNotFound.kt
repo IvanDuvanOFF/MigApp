0d879b78e26395a50e1d6879023d9b99b2f71034
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2024-06-04 15:39:53 +0700

    Написана документация 2

--- backend/src/main/kotlin/org/example/migapi/auth/exception/RevokedTokenNotFound.kt
+++
@@ -1,7 +0,0 @@
-package org.example.migapi.auth.exception
-
-import org.example.migapi.core.config.exception.MigApplicationException
-import org.springframework.http.HttpStatus
-
-class RevokedTokenNotFound(message: String = "Token not found") :
-    MigApplicationException(HttpStatus.BAD_REQUEST, message)
--- backend/src/main/kotlin/org/example/migapi/auth/exception/TfaNotEnabledException.kt
+++
@@ -1,7 +0,0 @@
-package org.example.migapi.auth.exception
-
-import org.example.migapi.core.config.exception.MigApplicationException
-import org.springframework.http.HttpStatus
-
-class TfaNotEnabledException :
-    MigApplicationException(HttpStatus.FORBIDDEN, "Two factor authentication not allowed for this user")
